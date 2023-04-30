ec3eea67a5181ef3ccfd250f3d895cd63785d7eb
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-04-30 22:33:30 +0300

    header was created

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import mealsImg from '../../assets/img/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+
+
+function Header() {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>PizzaLove</h1>
+                <HeaderCartButton></HeaderCartButton>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImg} alt="meals-bg" />
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
